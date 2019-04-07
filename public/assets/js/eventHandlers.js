6025500485e1e50ae9c6f12013cafa670eb5cd6f
Brady Sheridan <brady.sherid@gmail.com>
2019-04-07 16:17:10 -0400

    logic tweaks

--- public/assets/js/eventHandlers.js
+++ public/assets/js/eventHandlers.js
@@ -1,24 +1,24 @@
 function handlePointClick(e) {
-  // if there's a popup open, close it
   if (mapState.activePopup) {
     mapState.activePopup.remove()
-    mapState.activePopup = null
   }
 
-  var feature = e.features[0]
-  var coordinates = feature.geometry.coordinates.slice()
-  var props = Object.assign({}, feature.properties, { accentColor: feature.layer.paint['circle-color'] })
-  var html = popupFor[feature.layer.id](props)
+  let feature = e.features[0]
+  let coordinates = feature.geometry.coordinates.slice()
+  let props = Object.assign({}, feature.properties, { accentColor: feature.layer.paint['circle-color'] })
+  let html = popupFor[feature.layer.id](props)
 
   // ensure that if the map is zoomed out such that multiple copies of the
   // feature are visible, the popup appears over the copy being pointed to
-  // while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
-  //   coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360
-  // }
+  if (e.lngLat) {
+    while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+      coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360
+    }  
+  }
 
   // define popup offset values
-  var offset = 10
-  var popupOffsets = {
+  let offset = 10
+  let popupOffsets = {
     'top': [0, offset],
     'top-left': [offset, offset],
     'top-right': [-offset, offset],
@@ -30,10 +30,14 @@ function handlePointClick(e) {
   }
 
   // render popup
-  var popup = new mapboxgl.Popup({offset: popupOffsets, className: 'popup-wrap'})
+  let popup = new mapboxgl.Popup({offset: popupOffsets, className: 'popup-wrap'})
     .setLngLat(coordinates)
     .setHTML(html)
     .addTo(map)
+    .on('close', () => {
+      // mapState.activePopup
+      // mapState.activePopup = null
+    })
 
   // update map state
   popup.layerId = feature.layer.id
